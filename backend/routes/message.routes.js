2a12431abc15e1bfb74034b529e359c59dc8b003
huzefaTariq1 <101399969+huzefaTariq1@users.noreply.github.com>
2024-04-21 07:53:22 -0700

    created get messages function

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,13 +1,11 @@
 import express from "express";
 //import { getMessages, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/:id", protectRoute, (req,res)=>{
-res.send("get chat")
-});
+router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
